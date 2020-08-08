3380c4aba48ffc70ed275685fe6ef6f1c6e6828b
Stuart Wu <a80233810@gmail.com>
2020-08-08 14:39:04 +0800

    Updata File ver.2

--- readreviews.py
+++ readreviews.py
@@ -1,5 +1,6 @@
 data = []
 count = 0
+count1 = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		count += 1
@@ -7,4 +8,12 @@ with open('reviews.txt', 'r') as f:
 		if count % 100000 == 0:
 			print(len(data))
 print('檔案讀取完畢,總共有', len(data), '筆資料。')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+avg = sum_len / len(data)
+avg = int(avg)
+print('平均留言長度為', avg, '個字元。')
+
 #print(line.strip())
